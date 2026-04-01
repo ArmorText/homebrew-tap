b435c44da68b7f204ab3974294ac0c5ae4f108f7
Alex Rudnev <alex.rudnev@gmail.com>
2026-04-01 17:55:25 -0400

    Fix URL interpolation in armortext-desktop.rb

--- Casks/armortext-desktop.rb
+++ Casks/armortext-desktop.rb
@@ -2,7 +2,7 @@ cask "armortext-desktop" do
   version "0.35.15"
   sha256 "555b1d149565cbee372fc009967cb6c4cfa261315e40279013c74c9c16921e30"
 
-  url "https://downloads.armortext.com/desktop/release/${version}/ArmorText-${version}-mac64.dmg"
+  url "https://downloads.armortext.com/desktop/release/#{version}/ArmorText-#{version}-mac64.dmg"
   name "ArmorText"
   desc "ArmorText Desktop Messaging Client"
   homepage "https://armortext.com"
